0349894d43dc0d86f76780465f77f4899f39edcb
coralie <coralie.rohmer@hotmail.fr>
2020-07-21 15:20:40 +0200

    Script clustal format to fasta format

---
+++ clustalToFasta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys,re,os
+from Bio import SeqIO
+
+print("hello1")
+try:
+  file_clustal=sys.argv[sys.argv.index("-in") + 1]
+except:
+  pass
+  print("No file clustal format. -in file")
+  sys.exit()
+
+try:
+  output_fasta=sys.argv[sys.argv.index("-out") + 1]
+except:
+  pass
+  print("No output. -out file")
+  sys.exit()
+
+records = SeqIO.parse(file_clustal, "clustal")
+count = SeqIO.write(records, output_fasta, "fasta")
